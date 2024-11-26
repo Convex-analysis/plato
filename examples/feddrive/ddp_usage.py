f4dfaec3b482da898230bae91337c25331c28d94
Convex-analysis <799447128@qq.com>
2024-11-26 00:11:40 -0800

    new file:   examples/feddrive/ddp_usage.py

---
+++ examples/feddrive/ddp_usage.py
@@ -0,0 +1,55 @@
+"""run.py:"""
+#!/usr/bin/env python
+import torch.distributed.pipeline as pp
+import torch.nn as nn
+import torch
+from torch.distributed.pipelining import ScheduleGPipe
+
+#genreate a Lenet model and split it into 2 stages
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.conv1(x))
+        x = torch.nn.functional.max_pool2d(x, 2)
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = torch.nn.functional.max_pool2d(x, 2)
+        x = x.view(-1, 16 * 5 * 5)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class LeNet_stage0(nn.Module):
+    def __init__(self):
+        super(LeNet_stage0, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.conv1(x))
+        x = torch.nn.functional.max_pool2d(x, 2)
+        x = torch.nn.functional.relu(self.conv2(x))
+        x = torch.nn.functional.max_pool2d(x, 2)
+        return x
+    
+class LeNet_stage1(nn.Module):
+    def __init__(self):
+        super(LeNet_stage1, self).__init__()
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 16 * 5 * 5)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
